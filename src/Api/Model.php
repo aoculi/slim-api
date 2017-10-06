4716bf37cc8516bdb92ebedaf73795dfaec6d6eb
Alexis Oculi <alexis.oculi@gmail.com>
2017-10-06 18:14:04 +0200

    Renamed and updated Model class updated version

---
+++ src/Api/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Api;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+abstract class Model extends EloquentModel
+{
+
+}
--- src/Api/Models.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Api;
-
-abstract class Models
-{
-
-}
